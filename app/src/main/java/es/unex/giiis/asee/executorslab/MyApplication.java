fb22bc08a4c07a162edb28047179cf4f5349b387
J. Pery <jperianey@alumnos.unex.es>
2020-11-20 17:56:57 +0100

    Fixed context bug

--- app/src/main/java/es/unex/giiis/asee/executorslab/MyApplication.java
+++ app/src/main/java/es/unex/giiis/asee/executorslab/MyApplication.java
@@ -3,10 +3,11 @@ package es.unex.giiis.asee.executorslab;
 import android.app.Application;
 
 public class MyApplication extends Application {
-
-    // Instance of AppContainer that will be used by all the Activities of the app
     public AppContainer appContainer;
-    public MyApplication(){
-       appContainer = new AppContainer(this);
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContainer = new AppContainer(this);
     }
 }
